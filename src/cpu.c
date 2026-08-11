b343197b67e9bc1372f3cb6c55d3c71a548d4c9b
gappy <szakosbalazs2007@gmail.com>
2026-08-11 14:41:16 +0200

    feat: implement opcodes 00E0, 00EE, 1NNN, 2NNN, 3XKK, 4XKK, 5XY0, 6XKK, 7XKK

--- src/cpu.c
+++ src/cpu.c
@@ -54,8 +54,68 @@ bool chip8_load_rom(chip8_t *cpu, const char *filename) {
 
 void chip8_cycle(chip8_t *cpu) {
     uint16_t opcode = (cpu->memory[cpu->PC] << 8) | cpu->memory[cpu->PC + 1];
-
-    // TODO: Decode
-
     cpu->PC += 2;
+
+    const uint8_t x = (opcode & 0x0F00) >> 8;
+    const uint8_t y = (opcode & 0x00F0) >> 4;
+    const uint8_t kk = opcode & 0x00FF;
+    const uint16_t nnn = opcode & 0x0FFF;
+
+    switch (opcode & 0xF000) {
+        case 0x0000:
+            switch (opcode) {
+                case 0x0E00: // 0E00: Clear screen
+                    memset(cpu->display, 0, sizeof(cpu->display));
+                    break;
+                case 0x00EE: // 00EE: Return from subroutine
+                    cpu->SP--;
+                    cpu->PC = cpu->stack[cpu->SP];
+                    break;
+                default:
+                    printf("Unknown opcode [0x0000]: 0x%04X\n", opcode);
+                    break;
+            }
+            break;
+
+        case 0x1000: // 1NNN: Jump to address NNN
+            cpu->PC = nnn;
+            break;
+
+        case 0x2000: // 2NNN: Call subroutine at NNN
+            cpu->stack[cpu->SP] = cpu->PC;
+            cpu->SP++;
+            cpu->PC = nnn;
+            break;
+
+        case 0x3000: // 3XKK: Skip next instruction if Vx = KK
+            if (cpu->V[x] == kk) {
+                cpu->PC += 2;
+            }
+            break;
+
+        case 0x4000: // 4XKK: Skip next instruction if Vx != KK
+            if (cpu->V[x] != kk) {
+                cpu->PC += 2;
+            }
+            break;
+
+        case 0x5000: // 5XY0: Skip next instruction if Vx == Vy
+            if (opcode & 0x000F) break; // Must end in 0
+            if (cpu->V[x] == cpu->V[y]) {
+                cpu->PC += 2;
+            }
+            break;
+
+        case 0x6000: // 6XKK: Set Vx = KK
+            cpu->V[x] = kk;
+            break;
+
+        case 0x7000: // 7XKK: Set Vx += KK
+            cpu->V[x] += kk;
+            break;
+
+        default:
+            printf("Unimplemented opcode: 0x%04X\n", opcode);
+            break;
+    }
 }
--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include "chip8.h"
 
-void test_initialization() {
+void test_initialization(void) {
     chip8_t cpu;
     chip8_init(&cpu);
 
@@ -10,21 +10,45 @@ void test_initialization() {
     assert(cpu.memory[CHIP8_FONTSET_START] == 0xF0);
 }
 
-void test_fetch_cycle() {
+void test_opcodes_6xkk_7xkk(void) {
     chip8_t cpu;
     chip8_init(&cpu);
 
-    cpu.memory[0x200] = 0xA2;
-    cpu.memory[0x201] = 0xF0;
+    cpu.memory[0x200] = 0x61;
+    cpu.memory[0x201] = 0x25;
+    chip8_cycle(&cpu);
+    assert(cpu.V[1] == 0x25);
+
+    cpu.memory[0x202] = 0x71;
+    cpu.memory[0x203] = 0x05;
+    chip8_cycle(&cpu);
+    assert(cpu.V[1] == 0x2A);
+}
+
+void test_call_and_return(void) {
+    chip8_t cpu;
+    chip8_init(&cpu);
+
+    cpu.memory[0x200] = 0x24;
+    cpu.memory[0x201] = 0x00;
+
+    cpu.memory[0x400] = 0x00;
+    cpu.memory[0x401] = 0xEE;
 
     chip8_cycle(&cpu);
+    assert(cpu.PC == 0x400);
+    assert(cpu.SP == 1);
+    assert(cpu.stack[0] == 0x202);
 
-    assert(cpu.PC == CHIP8_PROGRAM_START + 2);
+    chip8_cycle(&cpu);
+    assert(cpu.PC == 0x202);
+    assert(cpu.SP == 0);
 }
 
 int main(void) {
     test_initialization();
-    test_fetch_cycle();
+    test_opcodes_6xkk_7xkk();
+    test_call_and_return();
 
     printf("OK: All tests passed.\n");
     return 0;
